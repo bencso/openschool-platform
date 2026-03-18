d7c4aa2eb2b5c3fb6ff0dccef08e23924c461e40
Gabor Hemrich <hemrichg@pm.me>
2026-03-18 17:24:21 +0100

    run formatter

--- frontend/src/components/Layout.tsx
+++ frontend/src/components/Layout.tsx
@@ -32,10 +32,7 @@ export default function Layout() {
       <header className="site-header">
         <div className="container header-inner">
           <Link to="/" className="logo">
-
-              <img src="logo-full.svg" alt="logo" id="logo-full"></img>
-
-
+            <img src="logo-full.svg" alt="logo" id="logo-full"></img>
           </Link>
           <button className="hamburger" onClick={() => setMenuOpen(!menuOpen)} aria-label="Menu">
             &#9776;
